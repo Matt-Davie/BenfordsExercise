81a6199b1cc22b8e530aeb27a27576df57aa8b4c
Matt Davie <mattdavie36@gmail.com>
2021-02-22 22:10:38 +1300

    main inclusion, comment formatting

--- Benfords.py
+++ Benfords.py
@@ -1,58 +1,106 @@
-###Steps###
-#load file
-#get 1st non-zero integer digits
-#count each
-#divide by sum(counts) to get %
-#print
-#display benfords ratios
+###  Notes  ###
+#  load file
+#  get 1st non-zero integer digits
+#  count each
+#  divide by sum(counts) to get %
+#  print
+#  display benfords ratios
 
-#assumptions: values can be floats - this means -ve, -1<x<1, including zero
-#decide how to handle -ve: make +ve
-#decide how to handle zero : return 0
-#decide how to handle -1<x<1 : return 1st non-zero integer
+#  assumptions: values can be floats - this means -ve, -1<x<1, including zero
+#  decide how to handle -ve: make +ve
+#  decide how to handle zero : return 0
+#  decide how to handle -1<x<1 : return 1st non-zero integer
 
 import numpy as np
-import csv
+import csv 
+import os
+import sys
 
-inputpath = 'BenfordTestData.csv'
+def main(input_file = 'BenfordTestData.csv'):
+    """
+    The main execute goes here
+    Expects a file named: BenfordTestData.csv to be in the same
+    diractory to be used if no argument is passed when running
+    the script
+    """
+    #  Get system path of script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    #  Create absolute path of file
+    input_path = script_dir + '\\' + input_file
+    
+    with open(input_path, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        #  Remove CSV header
+        next(reader)
 
-def firstdigit(n): #finding the first non-zero integer digit
+        data = np.array(list(reader))
+
+        #  Get rid of index column
+        data=data[:,1:]
+
+        #  Get 1st entries
+        for i in range(data.shape[1]):
+            for j in range(data.shape[0]):
+                data[j,i]=firstdigit(float(data[j,i]))
+
+        #  Count
+        unique, counts = np.unique(data.flatten(), return_counts=True)
+        #  Trim zero as it is irrelevant for Benford's
+        if int(unique[0])==0:
+            counts=counts[1:]
+        total_count=np.sum(counts)
+
+        data_percentage=[(i/total_count)*100 for i in counts]
+        #  1dp
+        data_percentage=np.around(data_percentage,1)
+
+        #  Predicted values
+        benfords=[np.log10(1+1/i)*100 for i in range(1,10)]
+        #  1dp
+        benfords=np.around(benfords,1)
+
+        #  Raw data
+        #  print(data_percentage)
+        #  print(benfords)
+
+        #  Pretty data
+        print('[' + ', '.join(map(str, data_percentage)) + ']')
+        print('[' + ', '.join(map(str, benfords)) + ']')
+
+def firstdigit(n):
+    """
+    Finding the first non-zero integer digit
+    """
     if n<0:
-        n=n*(-1) #no more negatives
+        #  No more negatives
+        n=n*(-1)
     if n==0:
-        return int(0) #return zero if zero - not much can be done here, we'll skip it later.
+        #  Return zero if zero - not much can be done here, we'll skip it later.
+        return int(0) 
     if n<1:
-        for i in str(n)[2:]: #skip '0.' then iterate
+        #  Skip '0.' then iterate
+        for i in str(n)[2:]: 
             if int(i)>0:
-                return int(i) #return first non-zero for cases like 0.40
+                #  Return first non-zero for cases like 0.40
+                return int(i) 
     else:
-        return int(str(n)[0]) #just that first digit
+        #  Just that first digit
+        return int(str(n)[0])
 
-#load this file
-with open(inputpath, 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    headers = next(reader)
-    data = np.array(list(reader))
+if __name__ == '__main__':
+    """
+    Run this first
+    """
+    csv_filename = ''
+    # If there is an argument passed, store it
+    try:
+        csv_filename = sys.argv[1]
+    except:
+        pass
 
-    #get rid of index column
-    data=data[:,1:]
-
-    #get 1st entries
-    for i in range(data.shape[1]):
-        for j in range(data.shape[0]):
-            data[j,i]=firstdigit(float(data[j,i]))
-
-    #now to count
-    unique, counts = np.unique(data.flatten(), return_counts=True)
-    if int(unique[0])==0: #trim zero as it is irrelevant for Benford's
-        counts=counts[1:]
-    total_count=np.sum(counts)
-
-    data_percentage=[(i/total_count)*100 for i in counts]
-    data_percentage=np.around(data_percentage,1) #1dp
-
-    benfords=[np.log10(1+1/i)*100 for i in range(1,10)] #predicted values
-    benfords=np.around(benfords,1) #1dp
-
-    print(data_percentage)
-    print(benfords)
+    # If the filename ends with csv, use it for Benford's Law
+    # else use the default value
+    if csv_filename.endswith('.csv'):
+        main(csv_filename)
+    else:
+        main()
