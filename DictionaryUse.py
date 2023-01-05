67a1723fa1d4cc5568a27f904380e291a7f7a97b
Matt Davie <77470085+Matt-Davie@users.noreply.github.com>
2023-01-05 21:32:13 +1300

    Update DictionaryUse.py

--- DictionaryUse.py
+++ DictionaryUse.py
@@ -1,3 +1,36 @@
+import numpy as np
+import csv 
+import os
+import sys
+
+def main(input_file = 'BenfordTestData.csv'):
+    """
+    The main execute goes here
+    Expects a file named: BenfordTestData.csv to be in the same
+    diractory to be used if no argument is passed when running
+    the script
+    """
+    #  Get system path of script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    #  Create absolute path of file
+    input_path = script_dir + '\\' + input_file
+    
+    with open(input_path, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        #  Remove CSV header
+        next(reader)
+
+        data = np.array(list(reader))
+
+        #  Get rid of index column
+        data=data[:,1:]
+
+        #  Get 1st entries
+        for i in range(data.shape[1]):
+            for j in range(data.shape[0]):
+                data[j,i]=firstdigit(float(data[j,i]))
+
+
 stats = {}
 for i in items:
     if i in stats:
